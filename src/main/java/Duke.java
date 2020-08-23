7ab02c6eb20a81f598af22cd4e2f611f1ad2f494
kstonekuan <kingstonkuan@u.nus.edu>
2020-08-23 16:59:29 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,14 +13,32 @@ public class Duke {
         TextManager textManager = new TextManager();
         String line = null;
         Scanner in = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
+        // Greet upon starting
         textManager.printGreetMessage();
 
+        // Loop until the user inputs "bye"
         do {
+            // Print previous line, nothing if first iteration
             textManager.printMessage(line);
-            line = in.nextLine() + "\n";
-        } while (!line.equals("bye\n"));
+            line = in.nextLine();
 
+            if (line.equals("list")) {
+                // List the tasks with numbers
+                line = "";
+                for (int i = 0; i < taskCount; i++) {
+                    line += (i + 1) + ". " + tasks[i] + "\n";
+                }
+            } else {
+                // Add the task to the list
+                tasks[taskCount++] = line;
+                line = "added: " + line + "\n";
+            }
+        } while (!line.equals("added: bye\n"));
+
+        // Exit the program
         textManager.printExitMessage();
     }
 }
