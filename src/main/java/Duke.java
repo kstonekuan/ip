8054c7058b3f45f4710e5ef29f8fe0d279309e68
kstonekuan <kingstonkuan@u.nus.edu>
2020-09-01 23:10:00 +0800

    Add todo, deadline and event as commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,33 +19,71 @@ public class Duke {
         // Greet upon starting
         textManager.printGreetMessage();
 
+        boolean hasNotExited = true;
+
         // Loop until the user inputs "bye"
-        while(true) {
+        while(hasNotExited) {
             inputMessage = in.nextLine(); // get the user input
             outputMessage = ""; // reset the output message
 
-            if (inputMessage.equals("bye")) {
-                break;
-            } else if (inputMessage.equals("list")) {
-                // List the tasks with numbers
-                for (int i = 0; i < Task.getTaskCount(); i++) {
-                    outputMessage += (i + 1) + "." + tasks[i].getStatusIcon()
-                            + " " + tasks[i].getDescription() + "\n";
+            String[] inputWords = inputMessage.split(" ");
+            String command = inputWords[0];
+            String description = "";
+            if (inputWords.length > 1) {
+                description = inputWords[1];
+                for (int i = 2; i < inputWords.length; i++) {
+                    description = String.join(" ", description, inputWords[i]);
                 }
-            } else if (inputMessage.split(" ")[0].equals("done")) {
-                // Mark the task as done
-                int taskDoneIndex = Integer.parseInt(inputMessage.split(" ")[1]) - 1;
-                tasks[taskDoneIndex].markAsDone();
-
-                outputMessage = "Nice! I've marked this task as done:\n  " + tasks[taskDoneIndex].getStatusIcon()
-                        + " " + tasks[taskDoneIndex].getDescription() + "\n";
-            } else {
-                // Add the task to the list
-                tasks[Task.getTaskCount()] = new Task(inputMessage);
-                outputMessage = "added: " + inputMessage + "\n";
             }
 
-            textManager.printMessage(outputMessage);
+            switch (command) {
+            case "bye":
+                hasNotExited = false;
+                break;
+            case "list":
+                // List the tasks with numbers
+                outputMessage = "Here are the tasks in your list:" + System.lineSeparator();
+                for (int i = 0; i < Task.getTaskCount(); i++) {
+                    outputMessage += (i + 1) + "." + tasks[i] + System.lineSeparator();
+                }
+                textManager.printMessage(outputMessage);
+                break;
+            case "done":
+                // Mark the task as done
+                int taskDoneIndex = Integer.parseInt(description) - 1;
+                tasks[taskDoneIndex].markAsDone();
+
+                outputMessage = "Nice! I've marked this task as done:\n  " + tasks[taskDoneIndex] + "\n";
+                textManager.printMessage(outputMessage);
+                break;
+            case "todo":
+                // Add the task to the list
+                tasks[Task.getTaskCount()] = new ToDo(description);
+                outputMessage = "Got it. I've added this task:\n  " + tasks[Task.getTaskCount() - 1]
+                        + "\nNow you have " + Task.getTaskCount() + " tasks in the list.\n";
+                textManager.printMessage(outputMessage);
+                break;
+            case "deadline":
+                // Add the task to the list
+                String[] deadlineInputs = description.split(" /by ");
+                String deadlineDescription = deadlineInputs[0];
+                String doByMessage = deadlineInputs[1];
+                tasks[Task.getTaskCount()] = new Deadline(deadlineDescription, doByMessage);
+                outputMessage = "Got it. I've added this task:\n  " + tasks[Task.getTaskCount() - 1]
+                        + "\nNow you have " + Task.getTaskCount() + " tasks in the list.\n";
+                textManager.printMessage(outputMessage);
+                break;
+            case "event":
+                // Add the task to the list
+                String[] eventInputs = description.split(" /at ");
+                String eventDescription = eventInputs[0];
+                String eventAtMessage = eventInputs[1];
+                tasks[Task.getTaskCount()] = new Event(eventDescription, eventAtMessage);
+                outputMessage = "Got it. I've added this task:\n  " + tasks[Task.getTaskCount() - 1]
+                        + "\nNow you have " + Task.getTaskCount() + " tasks in the list.\n";
+                textManager.printMessage(outputMessage);
+                break;
+            }
         }
 
         // Exit the program
