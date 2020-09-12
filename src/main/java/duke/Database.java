21de8a0f7fb0a5a1146985e82bab3c1164a59ecc
kstonekuan <kingstonkuan@u.nus.edu>
2020-09-13 00:40:09 +0800

    Refactor into methods using SLAP

--- src/main/java/duke/Database.java
+++ src/main/java/duke/Database.java
@@ -33,47 +33,83 @@ public class Database {
 
     public static void saveTasks(Task[] tasks) throws IOException {
         ArrayList<String> tasksAsStringList = new ArrayList<>();
+
         for (int i = 0; i < Task.getTaskCount(); i++) {
             tasksAsStringList.add(tasks[i].toString());
         }
+
         Files.write(FILE_PATH, tasksAsStringList);
     }
 
     public static void loadTasks(Task[] tasks) throws IOException, DukeException {
-        if (!fileExists) {
-            Files.createDirectories(DIR_DATA_PATH);
-            Files.createFile(FILE_PATH);
-            fileExists = Files.exists(FILE_PATH);
-        }
+        createFileIfNotExists();
+
         ArrayList<String> tasksAsStringList = (ArrayList<String>) Files.readAllLines(FILE_PATH);
+
         for (String taskAsString: tasksAsStringList) {
             String description = taskAsString.substring(LOAD_TASK_DESCRIPTION_INDEX);
             String taskType = taskAsString.substring(LOAD_TASK_TYPE_INDEX_START, LOAD_TASK_TYPE_INDEX_END);
             String statusIcon = taskAsString.substring(LOAD_TASK_STATUS_INDEX_START, LOAD_TASK_STATUS_INDEX_END);
             Task task;
-            switch (taskType) {
-            case ToDo.TODO_ICON:
-                task = new ToDo(description);
-                break;
-            case Deadline.DEADLINE_ICON:
-                String[] deadlineInputs = description.split(LOAD_DEADLINE_BY_SEPARATOR);
-                String deadlineDescription = deadlineInputs[TextManager.INPUT_DEADLINE_INDEX_DESC];
-                String doByMessage = deadlineInputs[TextManager.INPUT_DEADLINE_INDEX_BY];
-                task = new Deadline(deadlineDescription, doByMessage.substring(0, doByMessage.length() - 1));
-                break;
-            case Event.EVENT_ICON:
-                String[] eventInputs = description.split(LOAD_EVENT_AT_SEPARATOR);
-                String eventDescription = eventInputs[TextManager.INPUT_EVENT_INDEX_DESC];
-                String eventAtMessage = eventInputs[TextManager.INPUT_EVENT_INDEX_AT];
-                task = new Event(eventDescription, eventAtMessage.substring(0, eventAtMessage.length() - 1));
-                break;
-            default:
-                throw new DukeException();
-            }
-            if (statusIcon.equals(Task.TICK_ICON)) {
-                task.markAsDone();
-            }
+
+            task = getTaskFromString(description, taskType);
+
+            loadDone(statusIcon, task);
+
             tasks[Task.getTaskCount() - 1] = task;
         }
+    }
+
+    private static Task getTaskFromString(String description, String taskType) throws DukeException {
+        switch (taskType) {
+        case ToDo.TODO_ICON:
+            return loadToDo(description);
+        case Deadline.DEADLINE_ICON:
+            return loadDeadline(description);
+        case Event.EVENT_ICON:
+            return loadEvent(description);
+        default:
+            throw new DukeException();
+        }
+    }
+
+    private static void loadDone(String statusIcon, Task task) {
+        if (statusIcon.equals(Task.TICK_ICON)) {
+            task.markAsDone();
+        }
+    }
+
+    private static void createFileIfNotExists() throws IOException {
+        if (!fileExists) {
+            Files.createDirectories(DIR_DATA_PATH);
+            Files.createFile(FILE_PATH);
+            fileExists = Files.exists(FILE_PATH);
+        }
+    }
+
+    private static Task loadToDo(String description) {
+        Task task;
+        task = new ToDo(description);
+        return task;
+    }
+
+    private static Task loadEvent(String description) {
+        Task task;
+        String[] eventInputs = description.split(LOAD_EVENT_AT_SEPARATOR);
+        String eventDescription = eventInputs[TextManager.INPUT_EVENT_INDEX_DESC];
+        String eventAtMessageWithBracket = eventInputs[TextManager.INPUT_EVENT_INDEX_AT];
+        String eventAtMessage = eventAtMessageWithBracket.substring(0, eventAtMessageWithBracket.length() - 1);
+        task = new Event(eventDescription, eventAtMessage);
+        return task;
+    }
+
+    private static Task loadDeadline(String description) {
+        Task task;
+        String[] deadlineInputs = description.split(LOAD_DEADLINE_BY_SEPARATOR);
+        String deadlineDescription = deadlineInputs[TextManager.INPUT_DEADLINE_INDEX_DESC];
+        String doByMessageWithBracket = deadlineInputs[TextManager.INPUT_DEADLINE_INDEX_BY];
+        String doByMessage = doByMessageWithBracket.substring(0, doByMessageWithBracket.length() - 1);
+        task = new Deadline(deadlineDescription, doByMessage);
+        return task;
     }
 }
