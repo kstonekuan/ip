9d90943fa457d6ae1db17c00a2a68c019f4e3230
kstonekuan <kingstonkuan@u.nus.edu>
2020-09-13 00:28:16 +0800

    Refactor magic number indexes into constants

--- src/main/java/duke/Database.java
+++ src/main/java/duke/Database.java
@@ -19,7 +19,14 @@ public class Database {
     // works on Windows
     private static final Path DIR_DATA_PATH = Paths.get(DIR_CURRENT_PATH, "data");
     private static final Path FILE_PATH = Paths.get(DIR_CURRENT_PATH, "data", "duke.text");
-    private static boolean FILE_EXISTS = Files.exists(FILE_PATH);
+    private static final String LOAD_DEADLINE_BY_SEPARATOR = " \\(by: ";
+    public static final String LOAD_EVENT_AT_SEPARATOR = " \\(at: ";
+    public static final int LOAD_TASK_DESCRIPTION_INDEX = 7;
+    public static final int LOAD_TASK_TYPE_INDEX_START = 0;
+    public static final int LOAD_TASK_TYPE_INDEX_END = 3;
+    public static final int LOAD_TASK_STATUS_INDEX_START = 3;
+    public static final int LOAD_TASK_STATUS_INDEX_END = 6;
+    private static boolean fileExists = Files.exists(FILE_PATH);
 
     public Database() throws IOException {
     }
@@ -29,34 +36,33 @@ public class Database {
         for (int i = 0; i < Task.getTaskCount(); i++) {
             tasksAsStringList.add(tasks[i].toString());
         }
-        System.out.println(tasksAsStringList.get(0));
         Files.write(FILE_PATH, tasksAsStringList);
     }
 
     public static void loadTasks(Task[] tasks) throws IOException, DukeException {
-        if (!FILE_EXISTS) {
+        if (!fileExists) {
             Files.createDirectories(DIR_DATA_PATH);
             Files.createFile(FILE_PATH);
-            FILE_EXISTS = Files.exists(FILE_PATH);
+            fileExists = Files.exists(FILE_PATH);
         }
         ArrayList<String> tasksAsStringList = (ArrayList<String>) Files.readAllLines(FILE_PATH);
         for (String taskAsString: tasksAsStringList) {
-            String description = taskAsString.substring(7);
-            String taskType = taskAsString.substring(0, 3);
-            String statusIcon = taskAsString.substring(3, 6);
+            String description = taskAsString.substring(LOAD_TASK_DESCRIPTION_INDEX);
+            String taskType = taskAsString.substring(LOAD_TASK_TYPE_INDEX_START, LOAD_TASK_TYPE_INDEX_END);
+            String statusIcon = taskAsString.substring(LOAD_TASK_STATUS_INDEX_START, LOAD_TASK_STATUS_INDEX_END);
             Task task;
             switch (taskType) {
             case ToDo.TODO_ICON:
                 task = new ToDo(description);
                 break;
             case Deadline.DEADLINE_ICON:
-                String[] deadlineInputs = description.split(" \\(by: ");
+                String[] deadlineInputs = description.split(LOAD_DEADLINE_BY_SEPARATOR);
                 String deadlineDescription = deadlineInputs[TextManager.INPUT_DEADLINE_INDEX_DESC];
                 String doByMessage = deadlineInputs[TextManager.INPUT_DEADLINE_INDEX_BY];
                 task = new Deadline(deadlineDescription, doByMessage.substring(0, doByMessage.length() - 1));
                 break;
             case Event.EVENT_ICON:
-                String[] eventInputs = description.split(" \\(at: ");
+                String[] eventInputs = description.split(LOAD_EVENT_AT_SEPARATOR);
                 String eventDescription = eventInputs[TextManager.INPUT_EVENT_INDEX_DESC];
                 String eventAtMessage = eventInputs[TextManager.INPUT_EVENT_INDEX_AT];
                 task = new Event(eventDescription, eventAtMessage.substring(0, eventAtMessage.length() - 1));
